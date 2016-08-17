d07fa1b043f2b57c2bcee858f09a24befd10a371
Tyler Zeller <tylermzeller@gmail.com>
2016-08-16 22:26:53 -0400

    Change to object oriented design. Fix error with bottom and right snapback.

--- floaty.js
+++ floaty.js
@@ -1,117 +1,120 @@
-var mouse_over = false;
-var mouse_clicked = false;
-var snapback_interval = null;
-var x = 0, y = 0;
-var prevX, prevY;
+var floaty = floaty || {};
 
-document.addEventListener('DOMContentLoaded', function(e) {
-   var floater = document.getElementById('floaty');
-   updatePosition(floater);
-
-   floater.addEventListener('mouseover', function(e){
-     mouse_over = true;
-   });
-
-   floater.addEventListener('mousedown', function(e){
-     mouse_clicked = true;
-
-     addClass(floater, 'active');
-   });
-
-   floater.addEventListener('mouseup', function(e){
-     mouse_clicked = false;
-     removeClass(floater, 'active');
-
-    var direction = calcMinDirection(this);
-    snapback_interval = setInterval(snapback, 10, this, direction);
-   });
-
-   floater.addEventListener('mouseout', function(e){
-   });
-
-   floater.addEventListener('mousemove', function(e){
-     e.preventDefault();
-     if (mouse_clicked){
-       floater.style.left = e.clientX - (floater.clientWidth / 2) + 'px';
-       floater.style.top = e.clientY - (floater.clientHeight / 2) + 'px';
-
-       updatePosition(floater);
-     }
-   });
-});
-
-function snapback(element, direction){
-
-  var left = pixelToInt(element.style.left);
-  var top = pixelToInt(element.style.top);
-  var bottom = window.innerHeight - pixelToInt(element.style.top);
-  var right = window.innerWidth - pixelToInt(element.style.left);
-
-  var floaty_width = element.clientWidth;
-  var floaty_height = element.clientHeight;
-
-  if ((left <= 0.5) || (top <= 0.5) || (top + 50 >= window.innerHeight - 1) || (left + 50 >= window.innerWidth - 1) || mouse_clicked){
-    clearInterval(snapback_interval);
-  }
-
-  if (direction == 'left'){
-    x -= x / 10;
-    element.style.left = x + 'px';
-  }
-
-  if (direction == 'right') {
-    x += (window.innerWidth - 50 - x) / 10;
-    element.style.left = x + 'px';
-  }
-
-  if (direction == 'top') {
-    y -= y / 10;
-    element.style.top = y + 'px';
-  }
-
-  if (direction == 'bottom'){
-    y += (window.innerHeight - 50 - y) / 10;
-    element.style.top = y + 'px';
-  }
+floaty.pixelToInt = function(measurement){
+  var strings = measurement.split('px');
+  return parseFloat(strings[0]);
 }
 
-function calcMinDirection(element) {
-  var min = pixelToInt(element.style.left);
+floaty.floaty = function(dom_object){
+  this.element = dom_object;
+  this.mouse_over = false;
+  this.mouse_click = false;
+  this.snapback_interval = null;
+}
+
+floaty.floaty.prototype.updatePosition = function(mouseX, mouseY) {
+  this.element.style.left = mouseX - (this.element.clientWidth / 2) + 'px';
+  this.element.style.top = mouseY - (this.element.clientHeight / 2) + 'px';
+}
+
+floaty.floaty.prototype.addEventListener = function(eventName, callback){
+  this.element.addEventListener(eventName, callback);
+}
+
+floaty.floaty.prototype.removeClass = function(classname){
+  var re = new RegExp('(?:^|\\s)' + classname + '(?!\\S)', 'g');
+  this.element.className = this.element.className.replace( re , '' );
+}
+
+floaty.floaty.prototype.addClass = function(classname) {
+  this.element.className += ' ' + classname;
+}
+
+floaty.floaty.prototype.calcMinDirection = function(){
+  var x = floaty.pixelToInt(this.element.style.left);
+  var y = floaty.pixelToInt(this.element.style.top);
+
+  var min = x;
   var min_dir = 'left';
 
-  if (window.innerWidth - pixelToInt(element.style.left) < min) {
-    min = window.innerWidth - pixelToInt(element.style.left);
+  if (window.innerWidth - x < min) {
+    min = window.innerWidth - x;
     min_dir = 'right';
   }
 
-  if (pixelToInt(element.style.top) < 50){
+  if (y < 50){
     min_dir = 'top';
   }
 
-  if (window.innerHeight - pixelToInt(element.style.top) < 100) {
+  if (window.innerHeight - y < 100) {
     min_dir = 'bottom';
   }
 
   return min_dir;
 }
 
-function pixelToInt(measurement){
-  var strings = measurement.split('px');
-  return parseInt(strings[0]);
+floaty.floaty.prototype.snapback = function(floater, direction) {
+  var x = floaty.pixelToInt(floater.element.style.left);
+  var y = floaty.pixelToInt(floater.element.style.top);
+
+  var width = floater.element.clientWidth;
+  var height = floater.element.clientHeight;
+
+  if ((x <= 0.5 && x >= -0.5)
+  || (y <= 0.5 && y >= -0.5)
+  || (y + height >= window.innerHeight - 0.5 && y + height <= window.innerHeight + 0.5)
+  || (x + width >= window.innerWidth - 0.5 && x + width <= window.innerWidth + 0.5)
+  || floater.mouse_clicked){
+    clearInterval(floater.snapback_interval);
+  }
+
+  if (direction == 'left'){
+    x -= x / 10;
+    floater.element.style.left = x + 'px';
+  }
+
+  if (direction == 'right') {
+    x -= (x + width - window.innerWidth) / 10;
+    floater.element.style.left = x + 'px';
+  }
+
+  if (direction == 'top') {
+    y -= y / 10;
+    floater.element.style.top = y + 'px';
+  }
+
+  if (direction == 'bottom'){
+    y -= (y + height - window.innerHeight) / 10;
+    floater.element.style.top = y + 'px';
+  }
 }
 
-function updatePosition(element){
-  prevX = x;
-  prevY = y;
-  x = pixelToInt(element.style.left);
-  y = pixelToInt(element.style.top);
-}
+document.addEventListener('DOMContentLoaded', function(e) {
+   var floater = new floaty.floaty(document.getElementById('floaty'));
 
-function addClass(element, classname){
-  element.className += ' ' + classname;
-}
+   floater.addEventListener('mouseover', function(e){
+     floater.mouse_over = true;
+   });
 
-function removeClass(element, classname) {
-  var re = new RegExp('(?:^|\\s)' + classname + '(?!\\S)', 'g');
-  element.className = element.className.replace( re , '' );
-}
+   floater.addEventListener('mousedown', function(e){
+     floater.mouse_clicked = true;
+     floater.addClass('active');
+   });
+
+   floater.addEventListener('mouseup', function(e){
+     floater.mouse_clicked = false;
+     floater.removeClass('active');
+     var direction = floater.calcMinDirection();
+     floater.snapback_interval = setInterval(floater.snapback, 10, floater, direction);
+   });
+
+  //  floater.addEventListener('mouseout', function(e){
+  //  });
+
+   floater.addEventListener('mousemove', function(e){
+     e.preventDefault();
+     if (floater.mouse_clicked) {
+       floater.updatePosition(e.clientX, e.clientY);
+     }
+   });
+});
